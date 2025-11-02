bdfb09be2d3c46f9dceaecb9bc4dda004c8c4af5
anotherGU <lisicaalexander@gmail.com>
2025-11-02 19:47:19 +0200

    adssaasdasd

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-app.config['CSS_VERSION'] = '3'
+app.config['css_version'] = '3'
 
 @app.route('/')
 def index():
