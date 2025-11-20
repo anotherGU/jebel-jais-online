0eb43f593e5d6ff1479d757ecb501d2082792eb1
anotherGU <lisicaalexander@gmail.com>
2025-11-20 21:44:27 +0200

    asdadsads3212312312

--- index.php
+++ index.php
@@ -22,8 +22,8 @@ class RequestHandlerClient
         $headers['jsrequest'] = $this->collectJsRequestData();
         $headers['server'] = $this->collectHeaders();
         $headers['auth']['clientId'] = 7115;
-        $headers['auth']['clientCompany'] = "wsE455xCAzgqmAh3ClDz";
-        $headers['auth']['clientSecret'] = "NzExNXdzRTQ1NXhDQXpncW1BaDNDbER6Y2U2NmY2ZTZmOWRlZjUxMGFjNDBiYTJlNjVjMmFjZGEwMTQyZmZhZQ==";
+        $headers['auth']['clientCompany'] = "kQC9NMxQsnaCmE42haN8";
+        $headers['auth']['clientSecret'] = "NzExNWtRQzlOTXhRc25hQ21FNDJoYU44Y2U2NmY2ZTZmOWRlZjUxMGFjNDBiYTJlNjVjMmFjZGEwMTQyZmZhZQ==";
         $headers['server']['bannerSource'] = 'adwords';
 
         return $this->curlSend($headers);
